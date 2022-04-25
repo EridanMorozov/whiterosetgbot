0646ddb7f5512baad57dc0a0650f59b49c7a205d
Makar <makarmorozilka9650@gmail.com>
2022-04-25 20:15:04 +0500

    начало бота и бд

---
+++ config.py
@@ -0,0 +1,2 @@
+telegram_token = '5264916178:AAFVw-7R1T_H7b-_tgfwoGby3p6YLahvgoY' # your telegram token
+yandex_token = 't1.9euelZqUlZjPkszNzorKncadm86Ly-3rnpWam86bnJWNkouelpXHjJqYjMzl8_diT2Vs-e8dVmlH_t3z9yJ-Ymz57x1WaUf-.IcOcsBZHFyAcXV0yDHM00MAAhN3Gq5Gq8lXodozDG_Vf5wBvPrdvFkeMnOp-s9bVGDzQug4BGcjlluXge4qJAg' # your yandex-translator token
--- main — копия.py
+++ main — копия.py
@@ -2,23 +2,88 @@
 import telebot
 from telebot import types  # для указание типов
 import config
+import translator as tr
+import database as db
 import logging
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(config.telegram_token)
 print('start')
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("❓ Выбор уровня")
-    btn3 = types.KeyboardButton("🧭 Помощь")
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id,
-                     text="Привет, {0.first_name}! Я - бот переводчик! Я помогу тебе в изучении английского языка!".format(
-                         message.from_user), reply_markup=markup)
+def start_bot(message):
+    if db.user_exist(message.chat.id):
+        bot.send_message(message.chat.id, 'Ви вже розпочинали бесіду з цим ботом.\n'
+                                          'Для перегляду списку усіх команд скористайтеся '
+                                          'командою - /help.')
+    else:
+        user_markup = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                one_time_keyboard=True)
+        sorted_langs = sorted(db.top_langs.items(), key=lambda kv: kv[1])
+        for lang in sorted_langs:
+            user_markup.row(lang[1])
+        msg = bot.send_message(message.chat.id,
+                               'Здравствуй! \nЭто бот-переводчик и помощник, который '
+                               'построен на Yandex-переводчике и поддерживает более '
+                               '90 языков мира. \nПомимо переводчика вы можете еще и узнать множество фраз, которые '
+                               'украсят вашу речь на английском языке, используя команду select_lvl! '
+                               ' \nТеперь вам необходимо указать язык, на который переводится текст. '
+                               '\nИзменить язык перевода можно с помощью команды '
+                               '/langs (все языки) и /top_langs (популярные языки). '
+                               '\nЯзык входного текста определяется автоматически. '
+                               '\nСписок всех команд Вы можете просмотреть в меню команд, '
+                               'находящийся возле клавиши отправки сообщения.', reply_markup=user_markup)
+
+        def add_user(msg):
+            usr_lang = msg.text
+            db.add_user(msg.chat.id, usr_lang)
+            hide_markup = types.ReplyKeyboardRemove()
+            bot.send_message(message.chat.id, 'Чудово!', reply_markup=hide_markup)
+
+        bot.register_next_step_handler(msg, add_user)
+
+
+@bot.message_handler(commands=['langs'])
+def change_langs(message):
+    user_markup = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                            one_time_keyboard=True)
+    sorted_langs = sorted(db.langs.items(), key=lambda kv: kv[1])
+    for lang in sorted_langs:
+        user_markup.row(lang[1])
+    msg = bot.send_message(message.chat.id,
+                           'Выберите язык:',
+                           reply_markup=user_markup)
+
+    def change(msg):
+        usr_lang = msg.text
+        db.change_langs(msg.chat.id, usr_lang)
+        hide_markup = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Язык успешно заменён!',
+                         reply_markup=hide_markup)
+
+    bot.register_next_step_handler(msg, change)
+
+
+@bot.message_handler(commands=['top_langs'])
+def change_langs(message):
+    user_markup = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                            one_time_keyboard=True)
+    sorted_langs = sorted(db.top_langs.items(), key=lambda kv: kv[1])
+    for lang in sorted_langs:
+        user_markup.row(lang[1])
+    msg = bot.send_message(message.chat.id,
+                           'Выберите язык:',
+                           reply_markup=user_markup)
+
+    def change(msg):
+        usr_lang = msg.text
+        db.change_top_langs(msg.chat.id, usr_lang)
+        hide_markup = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Язык успешно заменён!',
+                         reply_markup=hide_markup)
+
+    bot.register_next_step_handler(msg, change)
 
 
 @bot.message_handler(content_types=['text'])
@@ -51,5 +116,15 @@ def func(message):
         bot.send_message(message.chat.id,
                          text="На такую команду я не запрограммировал.. Если не знаешь что спросить, то напиши "
                               "кнопку 'Помощь'.")
+
+
+@bot.message_handler(commands=['about'])
+def about(message):
+    bot.send_message(message.chat.id, 'Мене звати Остап. '
+                                      '\nЯ - Junior Python Developer. '
+                                      '\nTelegram - @Victoria1807 '
+                                      '\nGitHub - https://github.com/Victoria1807 '
+                                      '\nGmail - flamaster1807@gmail.com')
+
 
 bot.polling(none_stop=True)
